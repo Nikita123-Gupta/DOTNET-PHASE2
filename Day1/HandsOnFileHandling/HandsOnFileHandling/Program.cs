b0350921d1f6f59342e2f4026cecadba24c558dc
santuparsi <santuparsi@gmail.com>
2020-07-06 17:51:48 +0530

    initial

---
+++ Day1/HandsOnFileHandling/HandsOnFileHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace HandsOnFileHandling
+{
+    class Program
+    {
+        public static void Read(string path)
+        {
+            using(StreamReader sr=new StreamReader(path))
+            {
+                string content = sr.ReadToEnd();//reads all lines of text in the file 
+                Console.WriteLine(content);
+            }
+        }
+        static void Main(string[] args)
+        {
+           try
+            {
+                Console.WriteLine("Enter Path");
+                string path = Console.ReadLine();
+                Read(path);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
